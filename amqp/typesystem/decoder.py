13392d06485c51b6eba4058d5a855ea882ce72ac
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2016-06-12 04:30:17 +0200

    Add integer decoder

--- amqp/typesystem/decoder.py
+++ amqp/typesystem/decoder.py
@@ -1,3 +1,4 @@
+from amqp.utils import compat
 
 
 def decode_null(format_code, value):
@@ -7,3 +8,7 @@ def decode_null(format_code, value):
 
 def decode_string(encoding, format_code, value):
     return value.decode(encoding)
+
+
+def decode_integer(signed, format_code, value):
+    return compat.from_bytes(value, 'big', signed=signed)
--- amqp/typesystem/tests/test_decoder.py
+++ amqp/typesystem/tests/test_decoder.py
@@ -6,6 +6,7 @@ from amqp.typesystem import const
 from amqp.typesystem import decoder
 from amqp.utils import compat
 
+
 if sys.version_info.major == 2:
     bytes = bytearray
 
@@ -38,3 +39,76 @@ class StringDecoderTestCase(unittest.TestCase):
     def test_decode_str32(self):
         value = decoder.decode_string('utf-8', const.SYM8, self.b_str32)
         self.assertEqual(value, u'ë' * 256)
+
+
+class IntegerDecoderTestCase(unittest.TestCase):
+    b_ubyte = bytes([1])                # 1
+    b_ushort = bytes([1,0])             # 256
+    b_uint = bytes([1,0,0,0])           # 16777216
+    b_uint0 = bytes()                   # Should be decoded to 0, as an unsigned integer
+    b_smalluint = bytes([1])            # 1, decoded to an unsigned integer
+    b_ulong = bytes([1,0,0,0,0,0,0,0])  # 72057594037927936
+    b_ulong0 = bytes()                  # Should be decoded to 0, as an unsigned long integer
+    b_smallulong = bytes([1])           # 1, decoded to an unsigned long integer
+    b_byte = bytes([255])               # -1
+    b_short = bytes([255,0])            # -256
+    b_int = bytes([255,0,0,0])          # -1677216
+    b_smallint = bytes([255])           # -1
+    b_long = bytes([255,0,0,0,0,0,0,0]) # -72057594037927936
+    b_smalllong = bytes([255])          # -1
+
+    def test_decode_ubyte(self):
+        value = decoder.decode_integer(False, const.UBYTE, self.b_ubyte)
+        self.assertEqual(value, 1)
+
+    def test_decode_ushort(self):
+        value = decoder.decode_integer(False, const.USHORT, self.b_ushort)
+        self.assertEqual(value, 256)
+
+    def test_decode_uint(self):
+        value = decoder.decode_integer(False, const.UINT, self.b_uint)
+        self.assertEqual(value, 16777216)
+
+    def test_decode_uint0(self):
+        value = decoder.decode_integer(False, const.UINT0, self.b_uint0)
+        self.assertEqual(value, 0)
+
+    def test_decode_smalluint(self):
+        value = decoder.decode_integer(False, const.SMALLUINT, self.b_smalluint)
+        self.assertEqual(value, 1)
+
+    def test_decode_ulong(self):
+        value = decoder.decode_integer(False, const.ULONG, self.b_ulong)
+        self.assertEqual(value, 72057594037927936)
+
+    def test_decode_ulong0(self):
+        value = decoder.decode_integer(False, const.ULONG0, self.b_ulong0)
+        self.assertEqual(value, 0)
+
+    def test_decode_smallulong(self):
+        value = decoder.decode_integer(False, const.SMALLULONG, self.b_smallulong)
+        self.assertEqual(value, 1)
+
+    def test_decode_byte(self):
+        value = decoder.decode_integer(True, const.BYTE, self.b_byte)
+        self.assertEqual(value, -1)
+
+    def test_decode_short(self):
+        value = decoder.decode_integer(True, const.SHORT, self.b_short)
+        self.assertEqual(value, -256)
+
+    def test_decode_int(self):
+        value = decoder.decode_integer(True, const.INT, self.b_int)
+        self.assertEqual(value, -16777216)
+
+    def test_decode_smallint(self):
+        value = decoder.decode_integer(True, const.SMALLINT, self.b_smallint)
+        self.assertEqual(value, -1)
+
+    def test_decode_long(self):
+        value = decoder.decode_integer(True, const.LONG, self.b_long)
+        self.assertEqual(value, -72057594037927936)
+
+    def test_decode_smalllong(self):
+        value = decoder.decode_integer(True, const.SMALLLONG, self.b_smalllong)
+        self.assertEqual(value, -1)
